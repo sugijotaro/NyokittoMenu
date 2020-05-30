1bfbd26dca07cbf524dcadc7684bb1767f53aa60
sugijotaro <sugi.jotaro@gmail.com>
2020-05-31 00:11:59 +0900

    Initial Commit

---
+++ NyokittoMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NyokittoMenu
+//
+//  Created by JotaroSugiyama on 2020/05/31.
+//  Copyright © 2020 JotaroSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
